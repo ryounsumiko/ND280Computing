25dff5107c40006e09dbb00b03ab741cd04eefaf
Matthew Hogan <hoganman@gmail.com>
2018-04-12 21:12:50 +0900

    Adding a test ND280Custom DIRAC script

---
+++ processing_scripts/ND280Custom.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+from DIRAC.Interfaces.API.Dirac import Dirac
+from DIRAC.Interfaces.API.Job import Job
+
+exe = '/home/mhogan/software/ND280Computing/processing_scripts/P0DCalibProcess.py'
+args = '-v v12r15 -i lfn:/grid/t2k.org/nd280/raw/ND280/ND280/00014000_00014999/nd280_00014000_0000.daq.mid.gz'
+stdout = 'std.out'
+stderr = 'std.err'
+logfile = 'ND280Custom.log'
+
+# the executible here '' is later set, so don't confuse users later on
+diracJob = Job('', stdout, stderr)
+# give a descriptive name
+diracJob.setName('ND280Custom')
+# set the program/executable, arguments, logFile, ...
+diracJob.setExecutable(exe, arguments=args, logFile=logfile)
+# set the job length,5 hours
+diracJob.setCPUTime(18000)
+# this file is needed remotely for the job
+diracJob.setInputSandbox(['../tools/*py', '../custom_parameters/*DAT'])
+# these files are created by the job regardless of the executable
+diracJob.setOutputSandbox([stdout, stderr, logfile])
+"""
+Output data is written to / <VO> / user / <initial> / <username>
+so the full path of output data in this example is
+/<VO>/user/<initial>/<username>/tests
+
+"""
+diracJob.setOutputData(['LFN:*root'], outputSE='CA-TRIUMF-T2K1-disk', outputPath='tests')
+
+print 'job being submitted...'
+dirac = Dirac()
+result = dirac.submit(diracJob)
+print 'Submission Result: ', result

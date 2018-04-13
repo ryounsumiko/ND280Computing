89f2d91c52be83de41f888a6ca6be39e1766f747
Matthew Hogan <hoganman@gmail.com>
2018-04-13 13:46:31 +0900

    Added inputData

--- processing_scripts/ND280Custom.py
+++ processing_scripts/ND280Custom.py
@@ -5,26 +5,30 @@ Script.parseCommandLine()
 from DIRAC.Interfaces.API.Dirac import Dirac
 from DIRAC.Interfaces.API.Job import Job
 
+raw = 'lfn:/grid/t2k.org/nd280/raw/ND280/ND280/00014000_00014999/nd280_00014000_0000.daq.mid.gz'
 exe = '/home/mhogan/software/ND280Computing/processing_scripts/P0DCalibProcess.py'
-args = '-v v12r15 -i lfn:/grid/t2k.org/nd280/raw/ND280/ND280/00014000_00014999/nd280_00014000_0000.daq.mid.gz'
+args = '-v v12r15 -i %s' % raw
 stdout = 'std.out'
 stderr = 'std.err'
 logfile = 'ND280Custom.log'
-environmentDict = {
-    'VO_T2K_ORG_SW_DIR': '/cvmfs/t2k.egi.eu'
-}
-
-# the executible here '' is later set, so don't confuse users later on
-diracJob = Job('', stdout, stderr)
-# give a descriptive name
-diracJob.setName('ND280Custom')
-# set the program/executable, arguments, logFile, ...
-diracJob.setExecutable(exe, arguments=args, logFile=logfile)
-# set the job length
-diracJob.setCPUTime(3600)
-# this file is needed remotely for the job
-diracJob.setInputSandbox(
+# make sure to export VO_T2K_ORG_SW_DIR
+environmentDict =\
+    {
+        'VO_T2K_ORG_SW_DIR': '/cvmfs/t2k.egi.eu'
+    }
+# these files are larger than 10 MB
+inputData =\
     [
+        raw
+    ]
+outputData =\
+    [
+        'LFN:*root'
+    ]
+# this file is needed remotely for the job
+inputSandbox =\
+    [
+        exe,
         "../custom_parameters/P0DMOD.PARAMETERS.DAT",
         "RunCustom.py",
         "P0DCalibProcess.py",
@@ -36,20 +40,37 @@ diracJob.setInputSandbox(
         "../tools/pexpect.py",
         "../tools/StorageElement.py"
     ]
-)
+# these files are created by the job
+outputSandbox =\
+    [
+        stdout,
+        stderr,
+        logfile
+    ]
 
-# make sure to export VO_T2K_ORG_SW_DIR
+# the executible here '' is later set, so don't confuse users later on
+diracJob = Job('', stdout, stderr)
+
+# give a descriptive name
+diracJob.setName('ND280Custom')
+
+# set the program/executable, arguments, logFile, ...
+diracJob.setExecutable(exe, arguments=args, logFile=logfile)
+
+# set the job length
+diracJob.setCPUTime(3600)
+
+diracJob.setInputSandbox(inputSandbox)
 diracJob.setExecutionEnv(environmentDict)
+diracJob.setInputData(inputData)
+diracJob.setOutputSandbox(outputSandbox)
 
-# these files are created by the job regardless of the executable
-diracJob.setOutputSandbox([stdout, stderr, logfile])
 """
 Output data is written to / <VO> / user / <initial> / <username>
 so the full path of output data in this example is
 /<VO>/user/<initial>/<username>/tests
-
 """
-diracJob.setOutputData(['LFN:*root'], outputSE='CA-TRIUMF-T2K1-disk', outputPath='tests')
+diracJob.setOutputData(outputData, outputSE='CA-TRIUMF-T2K1-disk', outputPath='tests')
 
 print 'job being submitted...'
 dirac = Dirac()

b7ef668450197f7b9a29dec4cdbfdb8a5c8be627
ryounsumiko <has137@elm.phyast.pitt.edu>
2019-03-19 16:42:20 -0400

    fixed file transfer

--- processing_scripts/P0DBarEndCalib.py
+++ processing_scripts/P0DBarEndCalib.py
@@ -4,32 +4,30 @@ import os
 import sys
 import optparse
 
+from ND280GRID import ND280File,GetDefaultSE
 from ND280Job import ND280Job
 
-def appendLine(command,line):
-    return command + line+"\n"
-
-
 class P0DBarEndCalib(ND280Job):
-    """Run P0DBarEndCalib"""
-    def  __init__(self,nd280ver):
+    """Class for running bar-to-bar calibration.
+    """
+    def  __init__(self,nd280ver,input_file):
         super(P0DBarEndCalib,self).__init__(nd280ver)
         #Hard code intermidiate file name for simplicity.
         self.hitsOutput = "hits.root"
         self.inputToGenCalib = "file.txt"
+        self.nd280file = ND280File(input_file)
 
-    def CheckoutCodes(self):
-        # Export LIBRARY_PATH for searching path when compiling
+    def CheckoutCodes(self,ver="HEAD"):
         command =""
+        # Export LIBRARY_PATH for searching path when compiling
         command += "export LIBRARY_PATH=$LD_LIBRARY_PATH\n"
         # Check out cvs package.
-        command += "cmt checkout -r HEAD mppcCalib\n"
-        command += "pushd mppcCalib/*/cmt\n"
-        command += "cmt config\n"
+        command += "cmt checkout -r %s mppcCalib && " %(ver)
+        command += "pushd mppcCalib/*/cmt && "
+        command += "cmt config \n"
         # Do compiling
-        # Things may go wrong starting here
         command += "source ./setup.sh && "
-        command += "cmt make && "
+        command += "cmt make 2>/dev/null && "
         command += "which getP0DSandMuonBarEndHits.exe && "
         command += "popd; exit $?"
         rtc = self.RunCommand(command)
@@ -38,13 +36,15 @@ class P0DBarEndCalib(ND280Job):
             return False
         return True
 
-    def RunGetHits(self,input_filelist):
+    def RunGetHits(self):
+        # First, transfer input file to local. Might not be a good idea.
+        localfile = self.nd280file.CopyLocal(self.nd280file.path,GetDefaultSE())
         command =""
-        #Create input for next stage.
+        # Create input for next stage.
         command += "echo `pwd`/%s > %s\n" % (self.hitsOutput, self.inputToGenCalib)
         # Run getP0DSandMuonBarEndHits
         command += "source ./mppcCalib/*/cmt/setup.sh\n"
-        command += "getP0DSandMuonBarEndHits.exe -o %s %s" % (self.hitsOutput,input_filelist)
+        command += "getP0DSandMuonBarEndHits.exe -o %s %s" % (self.hitsOutput,localfile)
         rtc = self.RunCommand(command)
         if rtc:
             print("failed in executing getP0DSandMuonBarEndHits")
@@ -55,13 +55,13 @@ class P0DBarEndCalib(ND280Job):
         command =""
         #Run genP0DBarEndCalib.exe
         command += "source ./mppcCalib/*/cmt/setup.sh\n"
-        command += "genP0DBarEndCalib.exe -o %s %s" % (output_filename, self.hitsOutput)
+        #command += "genP0DBarEndCalib.exe -o %s %s" % (output_filename, self.hitsOutput)
         rtc = self.RunCommand(command)
         if rtc:
             print("failed in executing genP0DBarEndCalib")
             return False
         return True
-    
+
 def main(argv):
     # Parser Options
     ########################################################################
@@ -73,6 +73,7 @@ def main(argv):
     parser.add_option("-i", "--input", dest="input",
                       type="string",
                       help="Input to process, must be an lfn")
+    parser.add_option("-o", "--output", help="Name of output file", type="string")
     
     #parser.add_option("--useTestDB", action='store_true', default=False,
     #                  help="Prepend the DB cascade with the test DB")
@@ -86,10 +87,11 @@ def main(argv):
 
     # Main Program
     ########################################################################
-    P0Dcalib = P0DBarEndCalib(options.version)
+    P0Dcalib = P0DBarEndCalib(options.version,options.input)
     if not P0Dcalib.CheckoutCodes():
         return False
-    P0Dcalib.RunGetHits(options.input.replace("lfn:","lfn:/"))
+    P0Dcalib.RunGetHits()
+    P0Dcalib.RunGenerateCalib(options.output)
 
 
 if __name__ == "__main__":
